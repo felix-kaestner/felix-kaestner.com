5f63e3142afd19851fdd32bfd5c6f1fba5bdf9ad
Felix Kästner <kaestner.fe@gmail.com>
2020-11-22 21:06:39 +0100

    feat: add theme color meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ export default {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {name: 'theme-color', content: '#03a9f4'},
       {
         hid: 'description',
         name: 'description',
